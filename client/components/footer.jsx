080ba1467b6f8bf3e8b036185bfeb0982d500ee3
Phu Trieu <phutrieu95@gmail.com>
2021-05-13 00:37:20 -0700

    Completed footer component

--- client/components/footer.jsx
+++ client/components/footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="row justify-content-around mx-0">
-      <div className="mb-5 col-11 col-sm-3">
+      <div className="mb-5 col-11 col-md-3">
         <div className="d-flex py-2">
           <div className="logo me-2">
             <img src="/images/favicon-white.png" alt="Nano Center Logo" className="logo-img" />
@@ -19,17 +19,17 @@ const Footer = () => {
           <p>(123) 234-3456</p>
         </div>
       </div>
-      <div className="logo-text pt-2 mb-5 col-11 col-sm-3">
+      <div className="logo-text pt-2 mb-5 col-11 col-md-3">
         <h2 className="secondary-font m-0">Hours</h2>
         <p>Monday - Sunday</p>
         <p>10AM - 6PM</p>
       </div>
-      <div className="logo-text pt-2 mb-5 col-11 col-sm-3">
+      <div className="logo-text pt-2 mb-5 col-11 col-md-3">
         <h2 className="secondary-font m-0">Links</h2>
         <p><a href="">FAQ</a></p>
         <p><a href="">Privacy Policy</a></p>
       </div>
-      <div className="mb-3 col-11 col-sm-3">
+      <div className="mb-3 col-11 col-md-3">
         <h2 className="secondary-font pt-2">Follow Us</h2>
         <div className="sm-imgs">
           <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
